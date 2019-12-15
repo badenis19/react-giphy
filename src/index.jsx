95fe9bdf5bb5409a8a8c1b25e2bbd5ce47cb2bfc
Bruno ADENIS <adenis.bruno@hotmail.com>
2019-12-15 22:28:24 +0000

    toggle using setState working

--- src/index.jsx
+++ src/index.jsx
@@ -4,23 +4,39 @@ import ReactDOM from 'react-dom';
 import '../assets/stylesheets/application.scss';
 
 class Hello extends Component {
+
   constructor(props) {
-    super(props); // so that we don't break react
+    super(props); // so that we don't break React
     this.state = {
       clicked: false
     };
   }
 
+  handleClick = () => {
+    // change the state
+    this.setState({
+      clicked: !this.state.clicked
+    });
+  }
+
 
   render() {
-    //build and return HTML for the component
+    // render method is called by react automatically
+    // build and return HTML for the component
     return (
-      <div>Hello {this.props.name}</div>
-    )
+      // if the state of is true then we add the class clicked else nothing (TERNARY)
+      <div className={this.state.clicked ? 'clicked' : null}
+        onClick={this.handleClick}>
+        Hello {this.props.name}
+      </div>
+    );
   }
+
 }
 
 const root = document.getElementById('root');
 if (root) {
-  ReactDOM.render(<Hello name="Bruno" surname="ADENIS " />, root);
+  ReactDOM.render(
+    <Hello name="Bruno" />,
+     root);
 }
