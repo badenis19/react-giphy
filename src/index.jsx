a0d75a2c1cb4e174575de3f8bf50bbfdd9d716c7
Bruno ADENIS <adenis.bruno@hotmail.com>
2019-12-15 21:36:20 +0000

    first react component created

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
 
-const Hello = ({ name }) => {
-  return (
-    <div>
-      Hello,
-      {name}
-    </div>
-  );
-};
+class Hello extends Component {
+  constructor(props) {
+    super(props); // so that we don't break react
+    this.state = {
+      clicked: false
+    };
+  }
+
+
+  render() {
+    //build and return HTML for the component
+    return (
+      <div>Hello {this.props.name}</div>
+    )
+  }
+}
 
 const root = document.getElementById('root');
 if (root) {
-  ReactDOM.render(<Hello name="World" />, root);
+  ReactDOM.render(<Hello name="Bruno" surname="ADENIS " />, root);
 }
