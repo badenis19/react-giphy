4545e6fef177a9da9b9c197070b687c63367a5cf
Bruno ADENIS <adenis.bruno@hotmail.com>
2019-12-16 22:50:10 +0000

    first part complete

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
+// Components
 import SearchBar from './search_bar';
 import Gif from './gif';
+import GifList from './gif_list'
 
 class App extends Component {
   render() {
     return (
+      //Note: passing information to the Gif component
       <div>
         <div className="left-scene">
           <SearchBar />
           <div className="selected-gif">
-            <Gif />
+            <Gif id="gdNmEIkMm2wUpspdTf"/>
           </div>
         </div>
-        <div className="right-scene"></div>
+        <div className="right-scene">
+          <GifList />
+        </div>
       </div>
     );
   }
--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Gif extends Component {
   render() {
-    const src = "https://media0.giphy.com/media/gdNmEIkMm2wUpspdTf/200.gif";
+    const src = `https://media0.giphy.com/media/${this.props.id}/200.gif`;
     return (
       <img src={src} alt="" className="gif" />
     );
