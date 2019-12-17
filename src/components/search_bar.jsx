c90eef44538b5f6568e734480c704e7a575bfeb3
Bruno ADENIS <adenis.bruno@hotmail.com>
2019-12-17 11:25:18 +0000

    all components defined

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
 
 class SearchBar extends Component {
+
+  handleUpdate = (event) => {
+    console.log(event.target)
+  }
+
   render() {
     return (
-      <input type="text" className="form-control form-search" />
+      <input type="text" className="form-control form-search"
+        onUpdate={this.handleUpdate}/>
     );
   }
 
